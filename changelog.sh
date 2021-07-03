72ce464861635dcda0089e0e8263f961bf3a4aee
Siddharth <ahoy@barelyhuman.dev>
2021-07-03 17:25:02 +0530

    chore: remove changelog script

--- changelog.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash 
-
-file=commitlog-linux-amd64.tar.gz
-curl -L https://github.com/barelyhuman/commitlog/releases/latest/download/$file --output $file
-tar -xvf $file
-./commitlog > CHANGELOG.txt
